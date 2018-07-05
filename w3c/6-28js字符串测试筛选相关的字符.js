2106ff34f694c3fc43163428492a0a8c8180c311
261427834 <261427834@qq.com>
2018-07-05 11:25:10 +0800

    7-5

---
+++ w3c/6-28js字符串测试筛选相关的字符.js
@@ -0,0 +1,21 @@
+
+var list = document.querySelector('.output ul');
+list.innerHTML = '';
+var greetings = ['Happy Birthday!',
+                 'Merry Christmas my love',
+                 'A happy Christmas to all the family',
+                 'You\'re all I want for Christmas',
+                 'Get well soon'];
+
+for (var i = 0; i < greetings.length; i++) {
+  var input = greetings[i];
+  // Your conditional test needs to go inside the parentheses
+  // in the line below, replacing what's currently there
+  if (greetings[i].indexOf('Christmas')!==-1) {  
+     /*通过使用.indexOf(' ')!==-1;来选择出带有Christmas相关字符的语句。*/
+    var result = input;
+    var listItem = document.createElement('li');
+    listItem.textContent = result;
+    list.appendChild(listItem);
+  }
+}
---
+++ w3c/6-28将首字母转为大写其他字母为小写.js
@@ -0,0 +1,12 @@
+var list = document.querySelector('.output ul');
+list.innerHTML = '';
+var cities = ['lonDon', 'ManCHESTer', 'BiRmiNGHAM', 'liVERpoOL'];
+for(var i = 0; i < cities.length; i++) {
+  var input = cities[i];
+  // write your code just below here
+
+  var result = input;
+  var listItem = document.createElement('li');
+  listItem.textContent = result;
+  list.appendChild(listItem);
+}
